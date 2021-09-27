4ce2b398efd77c67a25198b6e32dcf75d0dd579a
Rashid Massanchi <rashid.massanchi@mail.utoronto.ca>
2021-09-27 13:54:40 -0400

    Added a new file Something.java, and made test for the file SomethingTest.java.

---
+++ src/main/java/Something.java
@@ -0,0 +1,18 @@
+import java.util.List;
+
+public class Something extends Trader implements Domesticatable {
+    /**
+     * Construct a Trader, randomly giving them both an inventory and
+     * a wishlist using the items in item_list.
+     *
+     * @param item_list A List of Tradable objects that may be placed in
+     *                  either this Trader's inventory or wishlist.
+     */
+
+
+
+    public String sound(){
+        return "brooom, vuvu";
+    }
+
+}
---
+++ src/test/java/SomethingTest.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.assertEquals;
+
+public class SomethingTest {
+    Something s;
+
+    public void TestSoundSomething() {
+        assertEquals("brooom, vuvu", s.sound());
+    }
+}
